a6f36d080c4a7fb9f26a980aecdea240613411b2
Zulu <jczuluaga@copaair.com>
2018-10-16 09:10:39 -0500

    Add product

--- Sales/Sales/ViewModels/AddProductViewModel.cs
+++ Sales/Sales/ViewModels/AddProductViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace Sales.ViewModels
 {
     using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
+    using System.Threading.Tasks;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
     using Helpers;
@@ -23,9 +26,27 @@
         private bool isRunning;
 
         private bool isEnabled;
+
+        private ObservableCollection<Category> categories;
+
+        private Category category;
         #endregion
 
         #region Properties
+        public List<Category> MyCategories { get; set; }
+
+        public Category Category
+        {
+            get { return this.category; }
+            set { this.SetValue(ref this.category, value); }
+        }
+
+        public ObservableCollection<Category> Categories
+        {
+            get { return this.categories; }
+            set { this.SetValue(ref this.categories, value); }
+        }
+
         public string Description { get; set; }
 
         public string Price { get; set; }
@@ -57,8 +78,56 @@
             this.apiService = new ApiService();
             this.IsEnabled = true;
             this.ImageSource = "noproduct";
+            this.LoadCategories();
         }
         #endregion
+
+        #region Methods
+        private async void LoadCategories()
+        {
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, connection.Message, Languages.Accept);
+                return;
+            }
+
+            var answer = await this.LoadCategoriesFromAPI();
+            if (answer)
+            {
+                this.RefreshList();
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+        }
+
+        private void RefreshList()
+        {
+            this.Categories = new ObservableCollection<Category>(this.MyCategories.OrderBy(c => c.Description));
+        }
+
+        private async Task<bool> LoadCategoriesFromAPI()
+        {
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlCategoriesController"].ToString();
+            var response = await this.apiService.GetList<Category>(url, prefix, controller, Settings.TokenType, Settings.AccessToken);
+            if (!response.IsSuccess)
+            {
+                return false;
+            }
+
+            this.MyCategories = (List<Category>)response.Result;
+            return true;
+        }
+        #endregion
+
 
         #region Commands
         public ICommand ChangeImageCommand
@@ -151,6 +220,15 @@
                 return;
             }
 
+            if (this.Category == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.CategoryError,
+                    Languages.Accept);
+                return;
+            }
+
             this.IsRunning = true;
             this.IsEnabled = false;
 
@@ -178,6 +256,8 @@
                 Price = price,
                 Remarks = this.Remarks,
                 ImageArray = imageArray,
+                CategoryId = this.Category.CategoryId,
+                UserId = MainViewModel.GetInstance().UserASP.Id,
             };
 
             var url = Application.Current.Resources["UrlAPI"].ToString();
