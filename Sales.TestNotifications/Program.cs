1620612245d79dadc91a4ed0b56a6e3ba1a938e5
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2018-10-25 14:13:52 -0500

    Test notificartions

---
+++ Sales.TestNotifications/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Sales.TestNotifications
+{
+    using System;
+    using Microsoft.Azure.NotificationHubs;
+
+    public class Program
+    {
+        private static NotificationHubClient hub;
+
+        public static void Main(string[] args)
+        {
+            hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://salesprep.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=xIAGRRWWWe7GNbkMVYmi67X3d+hjXAN/bWV96xmbmwQ=", "SalesPrep");
+
+            do
+            {
+                Console.WriteLine("Type a new message:");
+                var message = Console.ReadLine();
+                SendNotificationAsync(message);
+                Console.WriteLine("The message was sent...");
+            } while (true);
+        }
+
+        private static async void SendNotificationAsync(string message)
+        {
+            await hub.SendGcmNativeNotificationAsync("{ \"data\" : {\"Message\":\"" + message + "\"}}");
+        }
+
+    }
+}
